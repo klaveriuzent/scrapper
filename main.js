362b043c75c9fac7177bd4066570da10172a4c08
Jefry Chiedi <jefrychiedi1999@gmail.com>
2023-04-27 16:44:44 +0700

    add more handler

--- main.js
+++ main.js
@@ -3,12 +3,27 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const url = process.env.URL || 'https://www.detik.com';
+const cacheTime = process.env.CACHE_TIME || 1200000; // 20 menit
+
+let cachedData = null;
+let lastUpdated = null;
 
 async function getData() {
   try {
     // mengambil halaman web
-    const { data } = await axios.get('https://www.detik.com');
+    const { data, headers } = await axios.get(url, {
+      headers: {
+        'If-Modified-Since': lastUpdated || ''
+      }
+    });
+
+    // cek apakah data telah diperbarui
+    if (headers['last-modified'] === lastUpdated) {
+      console.log(`[${new Date().toLocaleTimeString()}] Data tidak diperbarui`);
+      return cachedData;
+    }
 
     // mengambil judul dan link artikel menggunakan cheerio
     const $ = cheerio.load(data);
@@ -23,6 +38,10 @@ async function getData() {
     const now = new Date();
     console.log(`[${now.toLocaleTimeString()} - ${now.toLocaleDateString()}] Data berhasil diambil`);
 
+    // menyimpan data yang diambil ke dalam cache
+    cachedData = articles;
+    lastUpdated = headers['last-modified'];
+
     // mengirimkan data yang diambil ke client dalam format JSON
     return articles;
   } catch (error) {
@@ -30,14 +49,14 @@ async function getData() {
   }
 }
 
-// menjalankan kode getData() setiap 20 menit
+// menjalankan kode getData() setiap cacheTime
 setInterval(async () => {
   try {
     const articles = await getData();
   } catch (error) {
     console.error(error);
   }
-}, 1200000);
+}, cacheTime);
 
 app.get('/', async (req, res) => {
   try {
